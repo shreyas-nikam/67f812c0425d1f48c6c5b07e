5db6f718feded18d9fbafdd0bb29c33b84206b3b
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-06-13 15:19:56 -0400

    Add main.py file importing all implementations

---
+++ main.py
@@ -0,0 +1,7 @@
+# Main file importing all implementations
+
+from definitions.None import None
+
+
+# This file serves as an entry point and makes all functions available from a single import
+# Example: from main import function_name
